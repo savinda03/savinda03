8af3c9628b0478132debac6362fe68d847b3528b
Savinda Rajapaksha <savindarajapaksha1045@gmail.com>
2023-11-26 23:35:42 +0530

    myJs

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js  gammak thamai</code> and save to reload.
         </p>
+        <h2>haane</h2>
+        <form action="" method="post">
+          <label for="uname">uname</label><br/>
+          <input type="text" className="text" /><br />
+          <label for="password">password</label><br />
+          <input type="text" className="text" />
+    
+        </form>
         <a
           className="App-link"
           href="https://reactjs.org"
